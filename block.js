a8830f5e67501279a1392eee09cab17601d33ab2
Nashrullah136 <73865894+Nashrullah136@users.noreply.github.com>
2023-07-23 15:26:33 +0700

    Update block.js

--- block.js
+++ block.js
@@ -2,7 +2,8 @@ var scs = document.querySelectorAll('script');
 var regex = /.*document\|getElementById\|if\|window\|(.*)\|contentLinkInfo/g;
 for(var i = 0; i < scs.length; i++) {
   var result = regex.exec(scs[i].text);
-  if (result !== null && result.length > 0){
-    console.log("adblock -> ", result);
+  if (result !== null){
+    console.log("adblock -> ", result[1]);
+    window[result[1]] = true;
   }
 }
