b580ee09440b611f7d6e7d64dfa94bc6be7ea020
Nashrullah136 <73865894+Nashrullah136@users.noreply.github.com>
2023-07-23 15:48:32 +0700

    Update block.js

--- block.js
+++ block.js
@@ -1,12 +1,10 @@
 /// block.js
 window.addEventListener("DOMContentLoaded", (event) => {
     var scs = document.querySelectorAll('script');
-    console.log("adblock -> ", scs);
     var regex = /.*`(.*)`/g;
     for (var i = 0; i < scs.length; i++) {
         var result = regex.exec(scs[i].text);
         if (result !== null) {
-            console.log("adblock -> ", result[1]);
             document.getElementById("contentLinkInfo").classList.add("d-none");
             document.getElementById("contentLink").innerHTML = atob(result[1]);
         }
