25cf071daff3a3909ff618c9ae70488c2cca434e
Nashrullah136 <73865894+Nashrullah136@users.noreply.github.com>
2023-07-23 15:23:09 +0700

    Update block.js

--- block.js
+++ block.js
@@ -1,5 +1,5 @@
-let scs = document.querySelectorAll('script');
-let regex = /.*document\|getElementById\|if\|window\|(.*)\|contentLinkInfo/g;
+var scs = document.querySelectorAll('script');
+var regex = /.*document\|getElementById\|if\|window\|(.*)\|contentLinkInfo/g;
 for(var i = 0; i < scs.length; i++) {
   var result = regex.exec(scs[i].text);
   console.log("adblock -> ", result);
